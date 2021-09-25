13a2cda43012b95b37c5b53c443c8920f2fa8573
nirmalknock <nirmalknock@gmail.com>
2021-09-25 14:15:05 +0530

    Updates

---
+++ precache-manifest.712a734b1516e3f3661074da648aff54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae91e4bc71f11384c312832a77c3b16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ebf50fc424f652b6dc",
+    "url": "/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "c58130e0e9e9b2a6bff7",
+    "url": "/static/js/2.43f8642f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.43f8642f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ebf50fc424f652b6dc",
+    "url": "/static/js/main.246aaadf.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b4710ee9ae851d5a4da94387e35e73ee",
+    "url": "/static/media/Neur.b4710ee9.jfif"
+  },
+  {
+    "revision": "2d2dd016dc082886da63fecd4736f79f",
+    "url": "/static/media/Nirmal_kumar_DP.2d2dd016.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "a729233b4e2021554074cb9a26770de2",
+    "url": "/static/media/nsic.a729233b.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
